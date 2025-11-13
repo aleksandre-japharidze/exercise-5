51e6b4f5d4e8fb4678ca1b1edfac4ebd373e2f51
AleksandreJapharidze <alexanderjapharidze@gmail.com>
2025-11-13 20:31:17 +0400

    TypeScript almost done

--- Typescript/src/index.ts
+++ Typescript/src/index.ts
@@ -43,8 +43,38 @@ function processText(text: string, operations: ((text: string) => string)[]) {
     return text;
 }
 
-console.log(trimText("   Hello World!   "));
-console.log(toUpperCase("hello world!"));
-console.log(censor("I am a job at a university. I am working hard to earn my salary."));
+// console.log(trimText("   Hello World!   "));
+// console.log(toUpperCase("hello world!"));
+// console.log(censor("I am a job at a university. I am working hard to earn my salary."));
+//
+// console.log(processText("Hello World! University and jobs!", [trimText, toUpperCase, censor, text => prefix(text, "Filtered: ")]));
 
-console.log(processText("Hello World! University and jobs!", [trimText, toUpperCase, censor, text => prefix(text, "Filtered: ")]));
+function createCensor(censoredWords: string[]) {
+    return (text: string) => {
+        if (text === null) {
+            return "";
+        }
+
+        for (const word of censoredWords) {
+            text = text.replace(new RegExp(word, "gi"), "*".repeat(word.length));
+        }
+
+        return text;
+    }
+}
+
+console.log(processText("Hello World! University and jobs!", [createCensor(["job", "salary", "university", "work"])]));
+
+function createPrefix(prefix: string) {
+    return (text: string) => {
+        if (text === null) {
+            return "";
+        } else if (prefix === null) {
+            return text;
+        }
+
+        return prefix + text;
+    }
+}
+
+console.log(processText("Hello World! University and jobs!", [createPrefix("Filtered: ")]));
